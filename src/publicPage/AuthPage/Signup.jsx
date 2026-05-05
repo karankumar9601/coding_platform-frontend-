cb25b067145b8062648558825946b7caffd705d4
karan kumar <karan@gmail.com>
2026-05-05 18:05:56 +0530

    connect backend to login and signup page and create AUTH Slice or Store

--- src/publicPage/AuthPage/Signup.jsx
+++ src/publicPage/AuthPage/Signup.jsx
@@ -2,22 +2,25 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod"
 import { useForm } from "react-hook-form"
 import { useNavigate } from "react-router"
+import { useDispatch, useSelector } from "react-redux";
+import { RegisterUser } from "../../store/auth_slice";
 
 
 const signupSchema = z.object({
     firstName: z.string().min(3, "firstName should contain atleast 3 character"),
-    email: z.string().email("Invalid Email"),
+    emailId: z.string().email("Invalid Email"),
     password: z.string().min(8, "password should contain atleast 8 character")
 })
 
 export default function Signup() {
     const navigate = useNavigate()
-    const { register, handleSubmit,reset, formState: { errors }, } = useForm({ resolver: zodResolver(signupSchema) });
+    const { register, handleSubmit, reset, formState: { errors }, } = useForm({ resolver: zodResolver(signupSchema) });
+    const { loading, user, error } = useSelector(state => state.auth)
+    const dispatch = useDispatch()
 
-    const signupData = (submittedData) => {
-      console.log(submittedData);
-      reset()
-      
+    const signupData =(submittedData) => {
+          dispatch(RegisterUser(submittedData))
+          reset()  
     }
 
     return (
@@ -32,30 +35,31 @@ export default function Signup() {
                     <form onSubmit={handleSubmit(signupData)} className="space-y-4">
                         <fieldset className="fieldset">
                             <legend className="fieldset-legend">First Name</legend>
-                            <input type="text"placeholder="Karan Kumar"className="input input-bordered w-full focus:outline-none focus:border-orange-500"{...register("firstName")}/>
+                            <input type="text" placeholder="Karan Kumar" className="input input-bordered w-full focus:outline-none focus:border-orange-500"{...register("firstName")} />
                             {errors.firstName && (<p className="text-error text-sm">{errors.firstName.message}</p>)}
                         </fieldset>
 
                         <fieldset className="fieldset">
                             <legend className="fieldset-legend">Email</legend>
-                            <input type="email"placeholder="karan@gmail.com"className="input input-bordered w-full focus:outline-none focus:border-orange-500"{...register("email")}/>
-                            {errors.email && (<p className="text-error text-sm">{errors.email.message}</p>)}
+                            <input type="email" placeholder="karan@gmail.com" className="input input-bordered w-full focus:outline-none focus:border-orange-500"{...register("emailId")} />
+                            {errors.emailId && (<p className="text-error text-sm">{errors.emailId.message}</p>)}
                         </fieldset>
 
                         <fieldset className="fieldset">
                             <legend className="fieldset-legend">Password</legend>
-                            <input type="password"placeholder="••••••••"className="input input-bordered w-full focus:outline-none focus:border-orange-500"{...register("password")}/>
+                            <input type="password" placeholder="••••••••" className="input input-bordered w-full focus:outline-none focus:border-orange-500"{...register("password")} />
                             {errors.password && (<p className="text-error text-sm">{errors.password.message}</p>)}
                         </fieldset>
 
-                        <button type="submit"className="btn w-full bg-gray-600 hover:bg- text-white border-none mt-2">Sign Up</button>
+                        <button type="submit" className="btn w-full bg-gray-600 hover:bg- text-white border-none mt-2">Sign Up</button>
+                        <span className="text-xl text-red-500">{user?.message}</span>
                     </form>
 
                     <div className="divider text-xs">OR</div>
 
                     <p className="text-center text-sm text-base-content/70">
                         Already have an account?
-                        <button type="button"className="link text-blue-600 ml-1"onClick={() => navigate("/login")}>Login</button>
+                        <button type="button" className="link text-blue-600 ml-1" onClick={() => navigate("/login")}>Login</button>
                     </p>
                 </div>
             </div>
